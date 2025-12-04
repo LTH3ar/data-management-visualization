f26610a4bd0c6baf952696ce83e5e9c80459240e
Akira Raven <letuanhuy1301@gmail.com>
2025-12-04 10:46:34 +0700

    update

--- test/pima/main.py
+++ test/pima/main.py
@@ -4,7 +4,7 @@ import sys
 
 # Method 1: Load from a CSV file (if you have it downloaded)
 # You can download from: https://www.kaggle.com/datasets/uciml/pima-indians-diabetes-database
-df = pd.read_csv(sys.argv[1])
+df = pd.read_csv(sys.argv[1], delimiter=",")
 
 # Or Method 2: Load from sklearn's datasets (if available)
 # from sklearn.datasets import load_diabetes
@@ -33,5 +33,5 @@ print(df.isnull().sum())  # Count of missing values per column
 print("\nColumn Names:")
 print(df.columns.tolist())
 
-print("\nValue Counts for Target Variable (Outcome):")
-print(df['Outcome'].value_counts())  # Distribution of target variable
+# print("\nValue Counts for Target Variable (Outcome):")
+# print(df['Outcome'].value_counts())  # Distribution of target variable
